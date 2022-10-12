6939a13b23170f216bdefe5ec4360d9886cd9463
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-12 16:39:05 +0200

    add images via js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /* Milestone 2: rimuovere tutto il markup statico e inserire tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript. */
 
-// 1. Creazione un array con i path delle immagini
+// 1. Creazione un array
 const images = [
         'img/01.webp',
         'img/02.webp',
@@ -9,11 +9,16 @@ const images = [
         'img/05.webp'
     ];
 
-// 2. stampo in console tutti path, singolarmente
+// 2. Seleziono l'elemento della dom dove collocare le immagini
 const containerEl = document.querySelector(".container");
 
+// 3. Creo una variabile per monitorare la slide attiva
+
 for (i = 0; i < images.length; i++){
-    containerEl.innerHTML = document.createElement = `${images}`;
+    
+    const containerElMarkup = `<img src="./assets/${images[i]}" alt="">`;
+    
+    containerEl.innerHTML += containerElMarkup;
 }
 
     
